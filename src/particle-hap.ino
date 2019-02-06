99d0aa640e08d77437dce666d5a7a39a3764a6d3
Lukas Jezny <ljezny@gmail.com>
2019-02-06 18:51:25 +0100

    - particle.ino fixed

--- src/particle-hap.ino
+++ src/particle-hap.ino
@@ -14,13 +14,9 @@ HKServer *hkServer = NULL;
 void setup() {
   Serial.begin();
   //while(!Serial.isConnected()) Particle.process();
-  byte mac[6];
-  WiFi.macAddress(mac);
-  String macString = String::format("%02X:%02X:%02X:%02X:%02X:%02X",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
-  Serial.println("Starting HKServer.");
-  //6C:0B:84:59:2E:DE
+
   Serial.println(macString.c_str());
-  hkServer = new HKServer("Particle1",macString.c_str(),"523-12-643");
+  hkServer = new HKServer("Particle1","523-12-643");
   hkServer->setup();
   initAccessorySet();
 
