35c0bad2666949f7a472f561d1413e9f25570a2b
Lukas Jezny <ljezny@gmail.com>
2019-02-10 21:39:06 +0100

    - covering

--- src/WindowsShutterAccessory.cpp
+++ src/WindowsShutterAccessory.cpp
@@ -22,9 +22,14 @@
 #define COVER_OPEN_TO_CLOSE_MS 60000
 #define TILT_OPEN_TO_CLOSE_MS 1000
 
-intCharacteristics *positionState;
-intCharacteristics *currentPosition;
-intCharacteristics *currentTiltAngle;
+intCharacteristics *positionStateChar;
+intCharacteristics *currentPositionChar;
+intCharacteristics *currentTiltAngleChar;
+
+//initially fully closed
+int state = 2;
+int position = 0;
+int tilt = -90;
 
 void shutterIdentity(bool oldValue, bool newValue, HKConnection *sender) {
     HKLogger.printf("Shutter identify\n");
@@ -36,6 +41,7 @@ void WindowsShutterAccessory::handle() {
 
 void setTargetPosition (int oldValue, int newValue, HKConnection *sender) {
     HKLogger.printf("setTargetPosition %d\n",newValue);
+    
 }
 
 void setTargetTiltAngle (int oldValue, int newValue, HKConnection *sender) {
@@ -57,24 +63,24 @@ void WindowsShutterAccessory::initAccessorySet() {
     shutterAccessory->addCharacteristics(windowsCoverService, nameCharacteristic);
 
     intCharacteristics *targetPosition = new intCharacteristics(charType_targetPosition, premission_read|premission_write|premission_notify, 0, 100, 1, unit_percentage);
-    targetPosition->characteristics::setValue("0");
+    targetPosition->characteristics::setValue(format("%d",position));
     targetPosition->valueChangeFunctionCall = &setTargetPosition;
     shutterAccessory->addCharacteristics(windowsCoverService, targetPosition);
 
-    currentPosition = new intCharacteristics(charType_currentPosition, premission_read|premission_notify, 0, 100, 1, unit_percentage);
-    currentPosition->characteristics::setValue("0");
-    shutterAccessory->addCharacteristics(windowsCoverService, currentPosition);
+    currentPositionChar = new intCharacteristics(charType_currentPosition, premission_read|premission_notify, 0, 100, 1, unit_percentage);
+    currentPositionChar->characteristics::setValue(format("%d",position));
+    shutterAccessory->addCharacteristics(windowsCoverService, currentPositionChar);
 
-    positionState = new intCharacteristics(charType_positionState, premission_read|premission_notify, 0, 2, 1, unit_percentage);
-    positionState->characteristics::setValue("2");
-    shutterAccessory->addCharacteristics(windowsCoverService, positionState);
+    positionStateChar = new intCharacteristics(charType_positionState, premission_read|premission_notify, 0, 2, 1, unit_percentage);
+    positionStateChar->characteristics::setValue(format("%d",state));
+    shutterAccessory->addCharacteristics(windowsCoverService, positionStateChar);
 
     intCharacteristics *targetHorizontalTiltAngle = new intCharacteristics(charType_targetHorizontalTiltAngle, premission_read|premission_write|premission_notify, -90, 0, 10, unit_arcDegree);
-    targetHorizontalTiltAngle->characteristics::setValue("0");
+    targetHorizontalTiltAngle->characteristics::setValue(format("%d",tilt));
     targetHorizontalTiltAngle->valueChangeFunctionCall = &setTargetTiltAngle;
     shutterAccessory->addCharacteristics(windowsCoverService, targetHorizontalTiltAngle);
     
-    currentTiltAngle = new intCharacteristics(charType_currentPosition, premission_read|premission_notify, 0, 100, 1, unit_percentage);
-    currentTiltAngle->characteristics::setValue("0");
-    shutterAccessory->addCharacteristics(windowsCoverService, currentTiltAngle);
+    currentTiltAngleChar = new intCharacteristics(charType_currentHorizontalTiltAngle, premission_read|premission_notify, -90, 0, 10, unit_arcDegree);
+    currentTiltAngleChar->characteristics::setValue(format("%d",tilt));
+    shutterAccessory->addCharacteristics(windowsCoverService, currentTiltAngleChar);
 };
