3de3d7dca84ae6f23739211c6058a1114314f09c
Lukas Jezny <ljezny@gmail.com>
2019-03-05 07:53:26 +0100

    external antena usage!

--- src/particle-hap.ino
+++ src/particle-hap.ino
@@ -23,12 +23,13 @@ void progress(Progress_t progress) {
     Serial.printf("PROGRESS: %d\n",progress);
 }
 
+STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
+
 // setup() runs once, when the device is first turned on.
 void setup() {
+
 	randomSeed(Time.now());//we need to somehow init random seed, so device identity will be unique
   Serial.begin();
-
-
 
   //HKPersistor().resetAll();
 
